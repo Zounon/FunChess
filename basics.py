e1cdbae233ef966570ef99ce2c77fbfb463c2280
Zounon <matthewsantos314@gmail.com>
2020-02-01 20:30:12 -0500

    playing with push_san

--- basics.py
+++ basics.py
@@ -1,7 +1,8 @@
-import chess 
+import chess
+board = chess.Board()
 
-brd = chess.Board()
+board.push_san("c2c4")
 
-print(brd.legal_moves)
-board = chess.Board("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
-print(board)
+board.push_san("e7e5")
+
+board
